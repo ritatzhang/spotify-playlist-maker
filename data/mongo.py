6e39e8f1c7c3984b601cc317395b778ae5f00b62
ritatzhang <ritatzhang@gmail.com>
2022-08-23 20:29:07 -0400

    debug

--- data/mongo.py
+++ data/mongo.py
@@ -7,11 +7,17 @@ dbname = 'spotify_app'
 mongoengine.connect(alias=alias_core,name=dbname,host=os.environ.get('MONGODB_URI', None))
 
 def store_user_auth(user_id, username, auth_token, auth_header):
+    print('IN STORE')
+    print(os.environ.get('MONGODB_URI', None))
     user = users.User()
     user.user_id = user_id
     user.username = username
     user.token = auth_token
     user.header = auth_header
-    user.save()
+    
+    try:
+        user.save()
+    except:
+        print("something went wrong")
 
     return user.id
